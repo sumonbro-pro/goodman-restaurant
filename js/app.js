d0be8cc0886119b05990e6ba80fd935c74330f14
Mausd Rana Sumon <mail.masudranasumon@gmail.com>
2024-01-08 23:22:34 +0600

    Some functional changes.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ ageInput.addEventListener('focusout', () => {
 
     getRes.addEventListener('click', () => {
         if (numValue <= 0) {
+            ageResHeading.innerHTML = `You're${numValue} 😂`
             ageResContainer.innerHTML = `Age can't be ${numValue} years old. 😂`;
         } else if (numValue < 18 && numValue > !17) {
             ageResHeading.textContent = `You're ${numValue} years old(a child), tables for you:`;
@@ -23,15 +24,17 @@ ageInput.addEventListener('focusout', () => {
                 ageResContainer.innerHTML += `<button class="tableStyle">${tables[i]}</button>`;
             }
         } else if (numValue < 100 && numValue >= 18) {
-            ageResHeading.textContent = `You're ${numValue} years old(an adult), tables for you:`;
+            ageResHeading.innerHTML = `You're ${numValue} years old(an adult), tables for you:`;
             let tables = ['VIP 1', 'VIP 2', 'VIP 3', 'VIP 4', 'VIP 5'];
             ageResContainer.innerHTML = '';
             for (let i = 0; i < tables.length; i++) {
                 ageResContainer.innerHTML += `<button class="tableStyle">${tables[i]}</button>`;
             }
         } else if (numValue >= 100 && numValue <= 120) {
+            ageResHeading.innerHTML = `Enough! You're already ${numValue}`;
             ageResContainer.innerHTML = `We don't allow ${numValue} years old.`;
         } else if (numValue > 120) {
+            ageResHeading.innerHTML = `Don't lie 😂`
             ageResContainer.innerHTML = `Don't lie that you're ${numValue} years old. 😂`;
         } else {
             ageInput.placeholder = 'Something wrong!';
